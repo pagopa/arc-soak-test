6037291105407af80d4d6e583cd57ba8c45c39d7
oleksiybozhykntt <oleksiy.bozhyk@nttdata.com>
2024-10-15 12:47:39 +0200

    added user info endpoint

--- src/api/auth.js
+++ src/api/auth.js
@@ -5,15 +5,26 @@ import { getBaseUrl, getInnerBaseUrl } from "../common/environment.js";
 
 export const AUTH_API_NAMES = {
   getAuthTokenTestUser: "transactions/getAuthTokenTestUser",
+  getUserInfo: "auth/getUserInfo",
 };
 
 const innerBaseUrl = getInnerBaseUrl();
+const baseUrl = getBaseUrl();
 
 export function getAuthTokenTestUser() {
   const apiName = AUTH_API_NAMES.getAuthTokenTestUser;
   const myParams = buildDefaultParams(apiName);
 
   const res = http.get(`${innerBaseUrl}/auth/testuser`, null, myParams);
+  logResult(apiName, res);
+  return res;
+}
+
+export function getUserInfo(token){
+  const apiName = AUTH_API_NAMES.getUserInfo;
+  const myParams = buildDefaultParams(apiName, token);
+
+  const res = http.get(`${baseUrl}/auth/user`, myParams);
   logResult(apiName, res);
   return res;
 }
