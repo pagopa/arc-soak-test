341da7bf5428447e0840fc6a336e6763bfdb5607
oleksiybozhykntt <oleksiy.bozhyk@nttdata.com>
2024-10-17 12:50:52 +0200

    fixed setup

--- src/tests/notices/getNoticeReceipt.js
+++ src/tests/notices/getNoticeReceipt.js
@@ -3,11 +3,13 @@ import {
   noticeReceipt,
   NOTICES_API_NAMES,
 } from "../../api/notices.js";
+
 import defaultHandleSummaryBuilder from "../../common/handleSummaryBuilder.js";
 import { defaultApiOptionsBuilder } from "../../common/dynamicScenarios/defaultOptions.js";
 import { logErrorResult } from "../../common/dynamicScenarios/utils.js";
 import { getAuthToken,abort,getTestEntity } from "../../common/utils.js";
 import { getNoticesList } from "./getNoticesList.js"
+import { setup } from "./getNoticeDetails.js"
 
 const application = "notices";
 const testName = "getNoticeReceipt";
@@ -24,19 +26,7 @@ export const options = defaultApiOptionsBuilder(
 // K6 summary configuration
 export const handleSummary = defaultHandleSummaryBuilder(application, testName);
 
-export function setup() {
-  const authToken = getAuthToken();
-  const noticesList = getNoticesList(authToken).json().notices;
-
-  if(noticesList.length === 0){
-    abort("No elements found in notice list please restart test with at least one element");
-  }
-
-  return {
-    token: authToken,
-    notices: noticesList.map(item => item.eventId),
-  };
-}
+export { setup };
 
 export default (data) => {
   const eventId = getTestEntity(data.notices);
